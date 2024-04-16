7df3c886949974f30a866cce3905add830791d33
K4sum1 <k4sum1@eclipse.cx>
2024-04-16 16:30:17 -0500

    stop-undesired-requests.patch from Librewolf

--- services/settings/Utils.sys.mjs
+++ services/settings/Utils.sys.mjs
@@ -51,6 +51,9 @@ ChromeUtils.defineLazyGetter(lazy, "isRunningTests", () => {
 // Overriding the server URL is normally disabled on Beta and Release channels,
 // except under some conditions.
 ChromeUtils.defineLazyGetter(lazy, "allowServerURLOverride", () => {
+
+  return true; // always override in LW
+
   if (!AppConstants.RELEASE_OR_BETA) {
     // Always allow to override the server URL on Nightly/DevEdition.
     return true;
