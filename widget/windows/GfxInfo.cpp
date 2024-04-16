98f5c17bff1c8970131cfde7bb8da4e868395704
K4sum1 <k4sum1@eclipse.cx>
2024-04-16 18:24:55 -0500

    Enable GPU acceleration on VM (thanks goodusername123)

--- widget/windows/GfxInfo.cpp
+++ widget/windows/GfxInfo.cpp
@@ -2081,6 +2081,9 @@ nsresult GfxInfo::GetFeatureStatusImpl(
             GfxDriverInfo::GetDeviceVendor(DeviceVendor::Microsoft),
             nsCaseInsensitiveStringComparator) &&
         !adapterVendorID.Equals(
+            GfxDriverInfo::GetDeviceVendor(DeviceVendor::MicrosoftHyperV),
+            nsCaseInsensitiveStringComparator) &&
+        !adapterVendorID.Equals(
             GfxDriverInfo::GetDeviceVendor(
                 DeviceVendor::MicrosoftRemoteDisplayAdapter),
             nsCaseInsensitiveStringComparator) &&
@@ -2090,6 +2093,12 @@ nsresult GfxInfo::GetFeatureStatusImpl(
         !adapterVendorID.Equals(
             GfxDriverInfo::GetDeviceVendor(DeviceVendor::Qualcomm),
             nsCaseInsensitiveStringComparator) &&
+        !adapterVendorID.Equals(
+            GfxDriverInfo::GetDeviceVendor(DeviceVendor::VMWare),
+            nsCaseInsensitiveStringComparator) &&
+        !adapterVendorID.Equals(
+            GfxDriverInfo::GetDeviceVendor(DeviceVendor::VirtualBox),
+            nsCaseInsensitiveStringComparator) &&
         // FIXME - these special hex values are currently used in xpcshell tests
         // introduced by bug 625160 patch 8/8. Maybe these tests need to be
         // adjusted now that we're only whitelisting intel/ati/nvidia.
@@ -2097,17 +2106,7 @@ nsresult GfxInfo::GetFeatureStatusImpl(
         !adapterVendorID.LowerCaseEqualsLiteral("0xdcba") &&
         !adapterVendorID.LowerCaseEqualsLiteral("0xabab") &&
         !adapterVendorID.LowerCaseEqualsLiteral("0xdcdc")) {
-      if (adapterVendorID.Equals(
-              GfxDriverInfo::GetDeviceVendor(DeviceVendor::MicrosoftHyperV),
-              nsCaseInsensitiveStringComparator) ||
-          adapterVendorID.Equals(
-              GfxDriverInfo::GetDeviceVendor(DeviceVendor::VMWare),
-              nsCaseInsensitiveStringComparator) ||
-          adapterVendorID.Equals(
-              GfxDriverInfo::GetDeviceVendor(DeviceVendor::VirtualBox),
-              nsCaseInsensitiveStringComparator)) {
-        aFailureId = "FEATURE_FAILURE_VM_VENDOR";
-      } else if (adapterVendorID.Equals(GfxDriverInfo::GetDeviceVendor(
+      if (adapterVendorID.Equals(GfxDriverInfo::GetDeviceVendor(
                                             DeviceVendor::MicrosoftBasic),
                                         nsCaseInsensitiveStringComparator)) {
         aFailureId = "FEATURE_FAILURE_MICROSOFT_BASIC_VENDOR";
