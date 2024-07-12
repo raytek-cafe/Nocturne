82f4f8c1cffe1e96f7aa1b7adf84d827e3667785
K4sum1 <k4sum1@eclipse.cx>
2024-07-12 08:28:26 -0500

    general.useragent.overide.(site) is bacc

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -504,6 +504,7 @@ class nsHttpChannel final : public HttpBaseChannel,
   [[nodiscard]] nsresult OpenCacheInputStream(nsICacheEntry* cacheEntry,
                                               bool startBuffering);
 
+  void SetLoadGroupUserAgentOverride();
   void SetOriginHeader();
   void SetDoNotTrack();
   void SetGlobalPrivacyControl();
--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -286,6 +286,24 @@ nsHttpHandler::nsHttpHandler()
   }
 }
 
+void nsHttpHandler::EnsureUAOverridesInit() {
+  MOZ_ASSERT(XRE_IsParentProcess());
+  MOZ_ASSERT(NS_IsMainThread());
+
+  static bool initDone = false;
+
+  if (initDone) {
+    return;
+  }
+
+  nsresult rv;
+  nsCOMPtr<nsISupports> bootstrapper =
+      do_GetService("@mozilla.org/network/ua-overrides-bootstrapper;1", &rv);
+  MOZ_ASSERT(bootstrapper);
+  MOZ_ASSERT(NS_SUCCEEDED(rv));
+  initDone = true;
+}
+
 nsHttpHandler::~nsHttpHandler() {
   LOG(("Deleting nsHttpHandler [this=%p]\n", this));
 
@@ -2079,6 +2097,11 @@ nsresult nsHttpHandler::SetupChannelInternal(
   }
 
   uint32_t caps = mCapabilities;
+
+  if (XRE_IsParentProcess()) {
+    // Load UserAgentOverrides.jsm before any HTTP request is issued.
+    EnsureUAOverridesInit();
+  }
 
   uint64_t channelId;
   nsresult rv = NewChannelId(channelId);
