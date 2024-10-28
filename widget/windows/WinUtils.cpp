0347323a7f3a5b24a59285243dcca5130140b77a
K4sum1 <k4sum1@eclipse.cx>
2024-10-28 14:25:02 +0100

    Vista workarounds to get it running

--- widget/windows/WinUtils.cpp
+++ widget/windows/WinUtils.cpp
@@ -1576,7 +1576,7 @@ uint32_t WinUtils::IsTouchDeviceSupportPresent() {
 
 /* static */
 uint32_t WinUtils::GetMaxTouchPoints() {
-  if (IsTouchDeviceSupportPresent()) {
+  if (IsWin7OrLater() && IsTouchDeviceSupportPresent()) {
     return GetSystemMetrics(SM_MAXIMUMTOUCHES);
   }
   return 0;
