763f265ecb6ed1c86157de5e40c552f3bc754c5d
K4sum1 <k4sum1@eclipse.cx>
2025-02-10 02:29:23 -0600

    Classic context menus preferences disabled by default
    Combined original commit and preference into one.
    
    Original description:
    At first I was probably going to drop this, but I've gotten used to it, and it's a lot better than the only able to open image in new tab in normal Firefox. Here you can view the image in the same tab, or middle click to open in a new tab in the background.

--- browser/base/content/browser-context.js
+++ browser/base/content/browser-context.js
@@ -117,6 +117,9 @@ document.addEventListener(
         case "context-media-showcontrols":
           gContextMenu.mediaCommand("showcontrols");
           break;
+        case "context-viewimagetab":
+        case "context-viewvideotab":
+        case "context-viewbgimage":
         case "context-viewimage":
         case "context-viewvideo":
           gContextMenu.viewMedia(event);
@@ -161,10 +164,14 @@ document.addEventListener(
           gContextMenu.setDesktopBackground();
           break;
         case "context-bookmarkpage":
+        case "context-bookmarkpage-old":
           gContextMenu.bookmarkThisPage();
           break;
         case "context-savepage":
           gContextMenu.savePageAs();
+          break;
+        case "context-sendpage":
+          gContextMenu.sendPage();
           break;
         case "context-pocket":
           Pocket.savePage(
@@ -299,6 +306,9 @@ document.addEventListener(
           break;
         case "context-viewsource":
           BrowserCommands.viewSource(gContextMenu.browser);
+          break;
+        case "context-viewinfo":
+          gContextMenu.viewInfo();
           break;
         case "context-inspect-a11y":
           gContextMenu.inspectA11Y();
