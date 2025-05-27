e0ae8fd469e5c1a4015feb62faa240061aba7e42
K4sum1 <k4sum1@eclipse.cx>
2025-05-27 10:56:39 -0500

    Add back Ogg Theora Video support

--- dom/media/platforms/agnostic/AgnosticDecoderModule.cpp
+++ dom/media/platforms/agnostic/AgnosticDecoderModule.cpp
@@ -6,6 +6,7 @@
 
 #include "AgnosticDecoderModule.h"
 
+#include "TheoraDecoder.h"
 #include "VPXDecoder.h"
 #include "mozilla/Logging.h"
 #include "mozilla/StaticPrefs_media.h"
@@ -23,6 +24,7 @@ enum class DecoderType {
   AV1,
 #endif
   Opus,
+  Theora,
   Vorbis,
   VPX,
   Wave,
@@ -35,6 +37,7 @@ static bool IsAvailableInDefault(DecoderType type) {
       return StaticPrefs::media_av1_enabled();
 #endif
     case DecoderType::Opus:
+    case DecoderType::Theora:
     case DecoderType::Vorbis:
     case DecoderType::VPX:
     case DecoderType::Wave:
@@ -52,6 +55,8 @@ static bool IsAvailableInRdd(DecoderType type) {
 #endif
     case DecoderType::Opus:
       return StaticPrefs::media_rdd_opus_enabled();
+    case DecoderType::Theora:
+      return StaticPrefs::media_rdd_theora_enabled();
     case DecoderType::Vorbis:
 #if defined(__MINGW32__)
       // If this is a MinGW build we need to force AgnosticDecoderModule to
@@ -82,7 +87,8 @@ static bool IsAvailableInUtility(DecoderType type) {
       return StaticPrefs::media_utility_vorbis_enabled();
     case DecoderType::Wave:
       return StaticPrefs::media_utility_wav_enabled();
-    // Others are video codecs, don't take care of them
+    case DecoderType::Theora:  // Video codecs, dont take care of them
+    case DecoderType::VPX:
     default:
       return false;
   }
@@ -122,7 +128,8 @@ media::DecodeSupportSet AgnosticDecoderModule::Supports(
       // something goes wrong with launching the RDD process.
       (AOMDecoder::IsAV1(mimeType) && IsAvailable(DecoderType::AV1)) ||
 #endif
-      (VPXDecoder::IsVPX(mimeType) && IsAvailable(DecoderType::VPX));
+      (VPXDecoder::IsVPX(mimeType) && IsAvailable(DecoderType::VPX)) ||
+      (TheoraDecoder::IsTheora(mimeType) && IsAvailable(DecoderType::Theora));
   MOZ_LOG(sPDMLog, LogLevel::Debug,
           ("Agnostic decoder %s requested type '%s'",
            supports ? "supports" : "rejects", mimeType.BeginReading()));
@@ -157,6 +164,9 @@ already_AddRefed<MediaDataDecoder> AgnosticDecoderModule::CreateVideoDecoder(
     }
   }
 #endif
+  else if (TheoraDecoder::IsTheora(aParams.mConfig.mMimeType)) {
+    m = new TheoraDecoder(aParams);
+  }
 
   return m.forget();
 }
