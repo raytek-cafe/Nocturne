fbef3824eec8a18d7979deb4bf9fb835a59f642a
K4sum1 <k4sum1@eclipse.cx>
2025-05-31 18:23:10 -0500

    Revert "Bug 1938683 - Don't support menulist-button as a proper widget. r=dshin"
    This reverts commit 30699bacafd23b8761c9e32943ce08940d6e9eda.

--- layout/forms/nsComboboxControlFrame.cpp
+++ layout/forms/nsComboboxControlFrame.cpp
@@ -273,15 +273,11 @@ static void printSize(char* aDesc, nscoord aSize) {
 
 bool nsComboboxControlFrame::HasDropDownButton() const {
   const nsStyleDisplay* disp = StyleDisplay();
-  switch (disp->EffectiveAppearance()) {
-    case StyleAppearance::MenulistButton:
-      return true;
-    case StyleAppearance::Menulist:
-      return !IsThemed(disp) ||
-             PresContext()->Theme()->ThemeNeedsComboboxDropmarker();
-    default:
-      return false;
-  }
+  // FIXME(emilio): Blink also shows this for menulist-button and such... Seems
+  // more similar to our mac / linux implementation.
+  return disp->EffectiveAppearance() == StyleAppearance::Menulist &&
+         (!IsThemed(disp) ||
+          PresContext()->Theme()->ThemeNeedsComboboxDropmarker());
 }
 
 nscoord nsComboboxControlFrame::DropDownButtonISize() {
