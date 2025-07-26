908446e74ec09e09d9af4364601a34a6a82de2fe
K4sum1 <k4sum1@eclipse.cx>
2025-07-26 13:29:48 -0500

    Revert "Bug 1756474 - Add locking to gfxPlatformFontList and gfxFontFamily, to allow use from worker threads. r=lsalzman 2022-04-19 22:20:21 7bf4d76a17cb009ff6b0ddfee4fd03e6073a9a1d"
    This reverts commit 6a9cbd7aa62461edc4ce05ef1bbb6d8fe47e5e1d.

--- gfx/thebes/gfxFontEntry.h
+++ gfx/thebes/gfxFontEntry.h
@@ -1117,6 +1117,7 @@ class gfxFontFamily {
 
   // mark this family as being in the "bad" underline offset blocklist
   void SetBadUnderlineFamily() {
+    mozilla::AutoWriteLock lock(mLock);
     mIsBadUnderlineFamily = true;
     if (mHasStyles) {
       SetBadUnderlineFonts();
