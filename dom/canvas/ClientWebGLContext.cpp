2157acb6b0c37d5e18eb92ad631cfce66ac1665f
Fatih Kilic <me@fklc.dev>
2025-08-13 18:50:30 +0000

    Bug 1980594 - Use `CanvasUtils::ImageExtraction` instead of `bool usePlaceholder`. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D259554

--- dom/canvas/ClientWebGLContext.cpp
+++ dom/canvas/ClientWebGLContext.cpp
@@ -1517,6 +1517,7 @@ RefPtr<gfx::DataSourceSurface> ClientWebGLContext::BackBufferSnapshot() {
 }
 
 UniquePtr<uint8_t[]> ClientWebGLContext::GetImageBuffer(
+    mozilla::CanvasUtils::ImageExtraction aExtractionBehavior,
     int32_t* out_format, gfx::IntSize* out_imageSize) {
   *out_format = 0;
   *out_imageSize = {};
@@ -1531,7 +1532,7 @@ UniquePtr<uint8_t[]> ClientWebGLContext::GetImageBuffer(
   const auto& premultAlpha = mNotLost->info.options.premultipliedAlpha;
   *out_imageSize = dataSurface->GetSize();
 
-  if (ShouldResistFingerprinting(RFPTarget::CanvasRandomization)) {
+  if (aExtractionBehavior == CanvasUtils::ImageExtraction::Randomize) {
     return gfxUtils::GetImageBufferWithRandomNoise(
         dataSurface, premultAlpha, GetCookieJarSettings(), PrincipalOrNull(),
         out_format);
@@ -1541,9 +1542,10 @@ UniquePtr<uint8_t[]> ClientWebGLContext::GetImageBuffer(
 }
 
 NS_IMETHODIMP
-ClientWebGLContext::GetInputStream(const char* mimeType,
-                                   const nsAString& encoderOptions,
-                                   nsIInputStream** out_stream) {
+ClientWebGLContext::GetInputStream(
+    const char* mimeType, const nsAString& encoderOptions,
+    mozilla::CanvasUtils::ImageExtraction spoofing,
+    nsIInputStream** out_stream) {
   // Use GetSurfaceSnapshot() to make sure that appropriate y-flip gets applied
   gfxAlphaType any;
   RefPtr<gfx::SourceSurface> snapshot = GetSurfaceSnapshot(&any);
