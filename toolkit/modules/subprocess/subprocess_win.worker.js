7fc01ef90026a88ac68d576ceeae03c6ff8056b7
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:41:05 -0600

    Bug 1856187 - Remove breakAwayFromJob from subprocess_win. r=Gijs 2023-10-03 03:00:08 91b29beb6f6ad727a592d26a518ff49d2a78c375

--- toolkit/modules/subprocess/subprocess_win.worker.js
+++ toolkit/modules/subprocess/subprocess_win.worker.js
@@ -565,6 +565,10 @@ class Process extends BaseProcess {
       win32.CREATE_SUSPENDED |
       win32.CREATE_UNICODE_ENVIRONMENT;
 
+    if (io.breakAwayFromJob) {
+      processFlags |= win32.CREATE_BREAKAWAY_FROM_JOB;
+    }
+
     let startupInfoEx = new win32.STARTUPINFOEXW();
     let startupInfo = startupInfoEx.StartupInfo;
 
@@ -761,6 +765,8 @@ io = {
       win32.HANDLE
     );
     this.updatePollEvents();
+
+    this.breakAwayFromJob = details.breakAwayFromJob;
 
     setTimeout(this.loop.bind(this), 0);
   },
