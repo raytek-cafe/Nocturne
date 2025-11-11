d40ccad3757f685cf69031ef5263ba50f31603d9
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:35:44 -0600

    Bug 1946066 - Simplify translucency setters. r=win-reviewers,rkraesig 2025-02-06 21:45:58 181ead9c41e05a1835c2e4220ef07b0f42a39d6e

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -2982,6 +2982,30 @@ void nsWindow::SetCursor(const Cursor& aCursor) {
 
 /**************************************************************
  *
+ * SECTION: nsIWidget::Get/SetTransparencyMode
+ *
+ * Manage the transparency mode of the window containing this
+ * widget.
+ *
+ **************************************************************/
+
+TransparencyMode nsWindow::GetTransparencyMode() {
+  return GetTopLevelWindow(true)->GetWindowTranslucencyInner();
+}
+
+void nsWindow::SetTransparencyMode(TransparencyMode aMode) {
+  nsWindow* window = GetTopLevelWindow(true);
+  MOZ_ASSERT(window);
+
+  if (!window || window->DestroyCalled()) {
+    return;
+  }
+
+  window->SetWindowTranslucencyInner(aMode);
+}
+
+/**************************************************************
+ *
  * SECTION: nsIWidget::UpdateWindowDraggingRegion
  *
  * For setting the draggable titlebar region from CSS
@@ -7186,13 +7210,22 @@ a11y::LocalAccessible* nsWindow::GetAccessible() {
 }
 #endif
 
-void nsWindow::SetTransparencyMode(TransparencyMode aMode) {
-  if (aMode == mTransparencyMode || DestroyCalled()) {
+/**************************************************************
+ **************************************************************
+ **
+ ** BLOCK: Transparency
+ **
+ ** Window transparency helpers.
+ **
+ **************************************************************
+ **************************************************************/
+
+void nsWindow::SetWindowTranslucencyInner(TransparencyMode aMode) {
+  if (aMode == mTransparencyMode) {
     return;
   }
 
   MOZ_ASSERT(WinUtils::GetTopLevelHWND(mWnd, true) == mWnd);
-  MOZ_ASSERT(GetTopLevelWindow(true) == this);
 
   mTransparencyMode = aMode;
 
