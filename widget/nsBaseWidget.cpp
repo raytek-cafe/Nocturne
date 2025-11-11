0ca350dc64ba039f70d13912c466b4215135b086
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:37:12 -0600

    Bug 1926624 - Unify top level widget behavior across platforms, standardizing on previous macOS behavior. r=stransky,mac-reviewers,win-reviewers,spohl,nrishel 2024-11-08 17:29:26 fbfcb95a50bd81fd0bcd472ac6e4db7a4d88d482

--- widget/nsBaseWidget.cpp
+++ widget/nsBaseWidget.cpp
@@ -562,14 +562,7 @@ void nsBaseWidget::Destroy() {
 
 nsIWidget* nsIWidget::GetTopLevelWidget() {
   auto* cur = this;
-  while (true) {
-    if (cur->IsTopLevelWidget()) {
-      break;
-    }
-    nsIWidget* parent = cur->GetParent();
-    if (!parent) {
-      break;
-    }
+  while (nsIWidget* parent = cur->GetParent()) {
     cur = parent;
   }
   return cur;
