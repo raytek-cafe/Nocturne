d0c47105f933a6addab7f0331d134e07086a63a0
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:38:33 -0600

    Bug 1892835 - Remove delayed NCACTIVATE hack. r=masayuki,win-reviewers,rkraesig 2024-06-05 00:24:44 17f924a32aa47e90d7246855c3c60728b0473794

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -593,6 +593,7 @@ class nsWindow final : public nsBaseWidget,
   static bool GetPopupsToRollup(
       nsIRollupListener* aRollupListener, uint32_t* aPopupsToRollup,
       mozilla::Maybe<POINT> aEventPoint = mozilla::Nothing());
+  static bool NeedsToHandleNCActivateDelayed(HWND aWnd);
   static bool DealWithPopups(HWND inWnd, UINT inMsg, WPARAM inWParam,
                              LPARAM inLParam, LRESULT* outResult);
 
@@ -879,7 +880,7 @@ class nsWindow final : public nsBaseWidget,
   class MOZ_STACK_CLASS ContextMenuPreventer final {
    public:
     explicit ContextMenuPreventer(nsWindow* aWindow)
-        : mWindow(aWindow), mNeedsToPreventContextMenu(false) {};
+        : mWindow(aWindow), mNeedsToPreventContextMenu(false){};
     ~ContextMenuPreventer() {
       mWindow->mNeedsToPreventContextMenu = mNeedsToPreventContextMenu;
     }
