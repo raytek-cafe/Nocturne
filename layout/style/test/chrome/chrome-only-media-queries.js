378d14a8503b2c9c3604bc36de7846355e18ce0d
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:43:02 -0600

    Bug 1845540 - Remove -moz-windows-default-theme and -moz-mac-graphite-theme. r=dao,pip-reviewers,mhowell 2023-07-27 02:22:26 d29139e526adac95455c13b9dc522ef387684e3f

--- layout/style/test/chrome/chrome-only-media-queries.js
+++ layout/style/test/chrome/chrome-only-media-queries.js
@@ -8,6 +8,8 @@ const CHROME_ONLY_TOGGLES = [
   "-moz-scrollbar-end-backward",
   "-moz-scrollbar-end-forward",
   "-moz-overlay-scrollbars",
+  "-moz-windows-default-theme",
+  "-moz-mac-graphite-theme",
   "-moz-mac-big-sur-theme",
   "-moz-menubar-drag",
   "-moz-windows-accent-color-in-titlebar",
--- widget/nsXPLookAndFeel.cpp
+++ widget/nsXPLookAndFeel.cpp
@@ -150,6 +150,8 @@ static const char sIntPrefs[][45] = {
     "ui.treeScrollLinesMax",
     "ui.chosenMenuItemsShouldBlink",
     "ui.windowsAccentColorInTitlebar",
+    "ui.windowsDefaultTheme",
+    "ui.macGraphiteTheme",
     "ui.windowsMica",
     "ui.windowsMicaPopups",
     "ui.macBigSurTheme",
