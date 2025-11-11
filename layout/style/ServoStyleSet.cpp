39045457608999b8a0f89ee925655aaaa1ca3bf5
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:34:50 -0600

    Bug 1955591 - Make LookAndFeel APIs safe to call off the main thread. r=jfkthame 2025-03-22 11:24:00 3618e5ed501243bf7f480192b862f4dfe116ca7a

--- layout/style/ServoStyleSet.cpp
+++ layout/style/ServoStyleSet.cpp
@@ -334,6 +334,8 @@ void ServoStyleSet::PreTraverseSync() {
 
   mDocument->ResolveScheduledPresAttrs();
 
+  LookAndFeel::NativeInit();
+
   mDocument->CacheAllKnownLangPrefs();
 
   if (gfxUserFontSet* userFontSet = mDocument->GetUserFontSet()) {
