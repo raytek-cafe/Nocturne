83983a5664ac2cec417b722c04df9dac85dffaf1
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:46:54 -0600

    Bug 1337299 - Remove old version compatibility into WinUtils. r=jimm 2017-02-10 11:06:23 bbf4de1ded24a95150f1dcb6929cea626fe6839f

--- widget/windows/TaskbarPreview.cpp
+++ widget/windows/TaskbarPreview.cpp
@@ -143,7 +143,7 @@ TaskbarPreview::Invalidate() {
   if (!gfxWindowsPlatform::GetPlatform()->DwmCompositionEnabled()) return NS_OK;
 
   HWND previewWindow = PreviewWindow();
-  return FAILED(DwmInvalidateIconicBitmaps(previewWindow)) ? NS_ERROR_FAILURE
+  return FAILED(WinUtils::dwmInvalidateIconicBitmapsPtr(previewWindow)) ? NS_ERROR_FAILURE
                                                            : NS_OK;
 }
 
@@ -276,10 +276,10 @@ WindowHook* TaskbarPreview::GetWindowHook() {
 
 void TaskbarPreview::EnableCustomDrawing(HWND aHWND, bool aEnable) {
   BOOL enabled = aEnable;
-  DwmSetWindowAttribute(aHWND, DWMWA_FORCE_ICONIC_REPRESENTATION, &enabled,
+  WinUtils::dwmSetWindowAttributePtr(aHWND, DWMWA_FORCE_ICONIC_REPRESENTATION, &enabled,
                         sizeof(enabled));
 
-  DwmSetWindowAttribute(aHWND, DWMWA_HAS_ICONIC_BITMAP, &enabled,
+  WinUtils::dwmSetWindowAttributePtr(aHWND, DWMWA_HAS_ICONIC_BITMAP, &enabled,
                         sizeof(enabled));
 }
 
@@ -374,10 +374,10 @@ TaskbarPreviewCallback::Done(nsISupports* aCanvas, bool aDrawBorder) {
   HRESULT hr;
   if (!mIsThumbnail) {
     POINT pptClient = {0, 0};
-    hr = DwmSetIconicLivePreviewBitmap(mPreview->PreviewWindow(), hBitmap,
+    hr = WinUtils::dwmSetIconicLivePreviewBitmapPtr(mPreview->PreviewWindow(), hBitmap,
                                        &pptClient, flags);
   } else {
-    hr = DwmSetIconicThumbnail(mPreview->PreviewWindow(), hBitmap, flags);
+    hr = WinUtils::dwmSetIconicThumbnailPtr(mPreview->PreviewWindow(), hBitmap, flags);
   }
   MOZ_ASSERT(SUCCEEDED(hr));
   mozilla::Unused << hr;
