f701b2c42766ecd5bcc474b8a553ca8bf86cb883
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:43:35 -0600

    Bug 1843663 - Remove pre windows-10-specific media queries and styles. r=dao,Gijs,handyman,settings-reviewers 2023-07-18 22:11:33 ab25174385891d01080da6e068572e8322125c00

--- layout/style/nsMediaFeatures.cpp
+++ layout/style/nsMediaFeatures.cpp
@@ -93,6 +93,11 @@ static nsSize GetDeviceSize(const Document& aDocument) {
   return pc->DeviceContext()->GetDeviceSurfaceDimensions();
 }
 
+bool Gecko_MediaFeatures_WindowsNonNativeMenus(const Document* aDocument) {
+  return LookAndFeel::WindowsNonNativeMenusEnabled() ||
+         aDocument->ShouldAvoidNativeTheme();
+}
+
 bool Gecko_MediaFeatures_IsResourceDocument(const Document* aDocument) {
   return aDocument->IsResourceDoc();
 }
@@ -251,6 +256,17 @@ bool Gecko_MediaFeatures_MatchesPlatform(StylePlatform aPlatform) {
 #if defined(XP_WIN)
     case StylePlatform::Windows:
       return true;
+    case StylePlatform::WindowsWin10:
+    case StylePlatform::WindowsWin7:
+    case StylePlatform::WindowsWin8: {
+      if (IsWin10OrLater()) {
+        return aPlatform == StylePlatform::WindowsWin10;
+      }
+      if (IsWin8OrLater()) {
+        return aPlatform == StylePlatform::WindowsWin8;
+      }
+      return aPlatform == StylePlatform::WindowsWin7;
+    }
 #elif defined(ANDROID)
     case StylePlatform::Android:
       return true;
