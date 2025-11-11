d3041ef85655a1acb79d99143f6304a0a76f59a5
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:41:42 -0600

    Bug 1844908 - Remove pre-Win10-specific codepath from gfx/. r=gfx-reviewers,nical 2023-08-17 10:58:21 29c7529fd8fa28f9ed07233615cbcb9386f35f9f

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -2465,7 +2465,8 @@ void gfxPlatform::InitAcceleration() {
       gfxCriticalNote << "Cannot evaluate keyed mutex feature status";
       gfxVars::SetAllowD3D11KeyedMutex(true);
     }
-    if (StaticPrefs::gfx_direct3d11_use_double_buffering()) {
+    if (StaticPrefs::gfx_direct3d11_use_double_buffering() &&
+        IsWin10OrLater()) {
       gfxVars::SetUseDoubleBufferingWithCompositor(true);
     }
 #endif
@@ -2846,7 +2847,8 @@ void gfxPlatform::InitWebRenderConfig() {
 
   bool useHwVideoZeroCopy = false;
   if (StaticPrefs::media_wmf_zero_copy_nv12_textures_AtStartup()) {
-    if (hasHardware) {
+    // XXX relax limitation to Windows 8.1
+    if (IsWin10OrLater() && hasHardware) {
       useHwVideoZeroCopy = true;
     }
 
