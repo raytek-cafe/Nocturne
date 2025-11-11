796482bcefbe3ca51ec93cd3571eb1ea2201cf27
K4sum1 <k4sum1@eclipse.cx>
2025-11-11 07:36:53 -0600

    Bug 1933958 - Add a test for the widget opaque region. r=gfx-reviewers,aosmond 2024-11-28 23:09:12 db3163e8ec64dc8430b1cd44130e35db4456e317

--- widget/nsIWidget.h
+++ widget/nsIWidget.h
@@ -1192,7 +1192,6 @@ class nsIWidget : public nsISupports {
    * @param aOpaqueRegion the region of the window that is opaque.
    */
   virtual void UpdateOpaqueRegion(const LayoutDeviceIntRegion& aOpaqueRegion) {}
-  virtual LayoutDeviceIntRegion GetOpaqueRegionForTesting() const { return {}; }
 
   /**
    * Informs the widget about the region of the window that is draggable.
