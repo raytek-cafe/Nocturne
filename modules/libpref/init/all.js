467671187054933ddbf7d7266bdcfcec11bd1d2d
the_r3dacted <k4sum1@eclipse.cx>
2025-12-10 23:29:32 -0600

    preferences or something

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3807,19 +3807,15 @@ pref("toolkit.aboutProcesses.profileDuration", 5);
 pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 pref("datareporting.policy.dataSubmissionEnabled", false);
+pref("datareporting.policy.dataSubmissionPolicyNotifiedTime", "32503679999000");
+pref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 999);
+pref("datareporting.policy.dataSubmissionPolicyBypassNotification", true);
 #ifdef MOZ_DATA_REPORTING
   pref("datareporting.policy.dataSubmissionEnabled", true);
-  pref("datareporting.policy.dataSubmissionPolicyNotifiedTime", "0");
-  pref("datareporting.policy.dataSubmissionPolicyAcceptedVersion", 0);
   pref("datareporting.policy.currentPolicyVersion", 2);
   pref("datareporting.policy.minimumPolicyVersion", 1);
   pref("datareporting.policy.minimumPolicyVersion.channel-beta", 2);
   pref("datareporting.policy.firstRunURL", "https://www.mozilla.org/privacy/firefox/");
-  #ifdef MOZILLA_OFFICIAL
-    pref("datareporting.policy.dataSubmissionPolicyBypassNotification", false);
-  #else
-    pref("datareporting.policy.dataSubmissionPolicyBypassNotification", true);
-  #endif
 #endif
 
 pref("datareporting.healthreport.uploadEnabled", false);
