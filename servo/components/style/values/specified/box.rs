07fc55a378d625eb808b833fdb86e29c998eb129
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-03-13 09:40:35 -0700

    moz-win-glass
    Revert "Bug 1798396 - Remove appearance: -moz-win-glass, which is unused. r=tnikkel"
    
    This reverts commit 1c8754699a903189615cd405826a530fb12c4b27.

--- servo/components/style/values/specified/box.rs
+++ servo/components/style/values/specified/box.rs
@@ -1665,6 +1665,9 @@ pub enum Appearance {
     MozWinMediaToolbox,
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     MozWinBrowsertabbarToolbox,
+    /// Vista glass.
+    #[parse(condition = "ParserContext::chrome_rules_enabled")]
+    MozWinGlass,
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     MozWinBorderlessGlass,
     /// -moz-apperance style used in setting proper glass margins.
--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -522,7 +522,8 @@ class nsWindow final : public nsBaseWidget,
   HRGN ExcludeNonClientFromPaintRegion(HRGN aRegion);
   static const wchar_t* GetMainWindowClass();
   bool HasGlass() const {
-    return mTransparencyMode == TransparencyMode::BorderlessGlass;
+    return mTransparencyMode == TransparencyMode::Glass ||
+           mTransparencyMode == TransparencyMode::BorderlessGlass;
   }
   HWND GetOwnerWnd() const { return ::GetWindow(mWnd, GW_OWNER); }
   bool IsOwnerForegroundWindow() const {
