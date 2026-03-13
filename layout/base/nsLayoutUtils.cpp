ce62c79bb442406d3d6e22c168a846339333283e
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-03-13 09:40:35 -0700

    moz-win-glass
    Revert "Bug 1798396 - Remove appearance: -moz-win-glass, which is unused. r=tnikkel"
    
    This reverts commit 1c8754699a903189615cd405826a530fb12c4b27.

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -6711,6 +6711,10 @@ widget::TransparencyMode nsLayoutUtils::GetFrameTransparency(
   StyleAppearance appearance =
       aCSSRootFrame->StyleDisplay()->EffectiveAppearance();
 
+  if (appearance == StyleAppearance::MozWinGlass) {
+    return TransparencyMode::Glass;
+  }
+
   if (appearance == StyleAppearance::MozWinBorderlessGlass) {
     return TransparencyMode::BorderlessGlass;
   }
