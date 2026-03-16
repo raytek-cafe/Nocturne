f143e5fb9d9f34805dc9280eb18eb6332c0eaac1
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-03-16 16:02:02 +0100

    FF68 Download Icons
    This makes the FF68 download icons optional by default instead of reverting them outright (r3dfox has them by default AND cannot be turned off). Might be useful to someone but I don't really know.

--- browser/components/preferences/nocturne.js
+++ browser/components/preferences/nocturne.js
@@ -55,6 +55,7 @@ for (let preference of [
   { id: "cookiebanners.service.mode.privateBrowsing", type: "int" },
   { id: "nocturne.legacyiconbehavior.enabled", type: "bool" },
   { id: "nocturne.smalliconbehavior.enabled", type: "bool" },
+  { id: "nocturne.ui.ff68downloadicons", type: "bool" },
   /* { id: "browser.translations.enable", type: "bool" },*/
 ]) {
   if (!Preferences.get(preference.id)) {
