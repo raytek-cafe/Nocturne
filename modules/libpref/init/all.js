447eae89a3b719c2b8672fc13760f0b74581d706
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-03-16 15:21:30 +0100

    Spoof Discord to Firefox 148

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -76,7 +76,7 @@ pref("general.useragent.site_specific_overrides", true);
 pref("general.useragent.override.aistudio.google.com", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0");
 pref("general.useragent.override.gemini.google.com", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0");
 pref("general.useragent.override.chase.com", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0");
-pref("general.useragent.override.discord.com", "Mozilla/5.0 (%OS_SLICE%; rv:140.0) Gecko/20100101 Firefox/140.0");
+pref("general.useragent.override.discord.com", "Mozilla/5.0 (%OS_SLICE%; rv:140.0) Gecko/20100101 Firefox/148.0");
 pref("general.useragent.override.gmx.com", "Mozilla/5.0 (%OS_SLICE%; rv:140.0) Gecko/20100101 Firefox/140.0");
 pref("general.useragent.override.ipcamlive.com", "Mozilla/5.0 (%OS_SLICE%; rv:140.0) Gecko/20100101 Firefox/140.0");
 pref("general.useragent.override.mail.com", "Mozilla/5.0 (%OS_SLICE%; rv:140.0) Gecko/20100101 Firefox/140.0");
