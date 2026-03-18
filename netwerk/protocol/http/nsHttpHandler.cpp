912e20f573bf0fbb4dac354add5740aa9f9f704e
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-03-18 23:18:30 +0100

    Update nsHttpHandler.cpp
    Makes it Nocturne\140.0 instead of Nocturne\140.9 (Sites will see it as a downgrade but it is consistent with Firefox's version number)

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -982,7 +982,7 @@ void nsHttpHandler::BuildUserAgent() {
     mUserAgent += ' ';
     mUserAgent += mAppName;
     mUserAgent += '/';
-    mUserAgent += mAppVersion;
+    mUserAgent.AppendLiteral(MOZILLA_UAVERSION);
   }
 }
 
