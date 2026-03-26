a0cf6689cc3efdd5cb60e54bd6d2a230e92ba9b4
ohfp <1813007-ohfp@users.noreply.gitlab.com>
2026-03-26 10:26:07 -0500

    hide-finish-setup-bookmark.patch from Librewolf

--- browser/components/asrouter/modules/OnboardingMessageProvider.sys.mjs
+++ browser/components/asrouter/modules/OnboardingMessageProvider.sys.mjs
@@ -2763,7 +2763,7 @@ const BASE_MESSAGES = () => [
     },
     priority: 3,
     targeting:
-      "'messaging-system-action.easyChecklist.open' | preferenceValue == true",
+      "false",
     trigger: {
       id: "preferenceObserver",
       params: ["messaging-system-action.easyChecklist.open"],
@@ -2967,7 +2967,7 @@ const BASE_MESSAGES = () => [
     },
     priority: 3,
     targeting:
-      "'messaging-system-action.easyChecklist.open' | preferenceValue == true",
+      "false",
     trigger: {
       id: "messagesLoaded",
     },
