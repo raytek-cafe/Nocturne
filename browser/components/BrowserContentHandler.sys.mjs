34122036b8557f86447319041685369e064a1f5f
the_r3dacted <k4sum1@eclipse.cx>
2026-03-27 10:57:21 -0500

    Extend JPEG XL detection/recognition from Waterfox
    Removed Rust overrides since the suggestion in eRust should* take care of it and I hate having it break every so often. fuck cargo

--- browser/components/BrowserContentHandler.sys.mjs
+++ browser/components/BrowserContentHandler.sys.mjs
@@ -1302,6 +1302,7 @@ function maybeRecordToHandleTelemetry(uri, isLaunch) {
       ".xhtml",
       ".svg",
       ".webp",
+      ".jxl",
     ]);
     if (registeredExtensions.has(extension)) {
       counter[extension].add(1);
