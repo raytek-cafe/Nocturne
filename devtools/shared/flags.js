687f0f733f809902073cd0df3ba6b0530054e84d
the_r3dacted <k4sum1@eclipse.cx>
2026-04-05 21:42:53 -0500

    devtools-bypass.patch from Librewolf

--- devtools/shared/flags.js
+++ devtools/shared/flags.js
@@ -67,3 +67,5 @@ makePrefTrackedFlag(exports, "wantVerbose", "devtools.debugger.log.verbose");
  * behaviors typically enable easier testing or enhanced debugging features.
  */
 makePrefTrackedFlag(exports, "testing", "devtools.testing");
+
+makePrefTrackedFlag(exports, "lwDetach", "librewolf.debugger.force_detach");
