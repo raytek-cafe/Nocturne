c15cec7c8107d2ae6581366e09df43575d329a49
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-04-13 01:49:11 +0200

    Give Windows 10 aero by default
    By default it will use the Pre-10 UI by default (with dcomp disabled as that causes issues on native styles.) and only re-enables both dcomp and the 10 UI if spoofed to 10.

--- widget/windows/nsNativeThemeWin.cpp
+++ widget/windows/nsNativeThemeWin.cpp
@@ -2186,8 +2186,8 @@ bool nsNativeThemeWin::GetWidgetPadding(nsDeviceContext* aContext,
 
   int overrideWinVer =
       StaticPrefs::widget_native_controls_override_win_version();
-  bool isWindows10OrLater =
-      (overrideWinVer == 0 && IsWin10OrLater()) || overrideWinVer >= 10;
+  // Nocturne: Only use Win10+ UI if explicitly spoofed as Win10+
+  bool isWindows10OrLater = overrideWinVer >= 10;
 
   switch (aAppearance) {
     // Radios and checkboxes return a fixed size in GetMinimumWidgetSize
