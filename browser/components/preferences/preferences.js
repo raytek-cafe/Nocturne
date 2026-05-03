b4d1741bb1a87bb472f7854c197dae4262842997
the_r3dacted <k4sum1@eclipse.cx>
2026-05-03 05:15:08 -0500

    Modified pref-pane-small.patch plus settings from Librewolf

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -9,6 +9,7 @@
 /* import-globals-from containers.js */
 /* import-globals-from translations.js */
 /* import-globals-from privacy.js */
+/* import-globals-from librewolf.js */
 /* import-globals-from sync.js */
 /* import-globals-from experimental.js */
 /* import-globals-from moreFromMozilla.js */
@@ -195,6 +196,7 @@ function init_all() {
   register_module("paneHome", gHomePane);
   register_module("paneSearch", gSearchPane);
   register_module("panePrivacy", gPrivacyPane);
+  register_module("paneLibrewolf", gLibrewolfPane);
   register_module("paneContainers", gContainersPane);
 
   if (Services.prefs.getBoolPref("browser.translations.newSettingsUI.enable")) {
