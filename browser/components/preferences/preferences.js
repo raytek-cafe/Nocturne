63dea0537d412facb7370a33f45943abf90a706a
the_r3dacted <k4sum1@eclipse.cx>
2026-05-03 04:51:19 -0500

    Librewolf remove-organization-policy-banner.patch

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -242,8 +242,6 @@ function init_all() {
     this.removeAttribute("keyboard-navigation");
   });
 
-  maybeDisplayPoliciesNotice();
-
   window.addEventListener("hashchange", onHashChange);
 
   document.getElementById("focusSearch1").addEventListener("command", () => {
