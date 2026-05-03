d198d8efacff189ba9727866be8d8e1033c20012
the_r3dacted <k4sum1@eclipse.cx>
2026-05-03 04:49:55 -0500

    Librewolf hide-passwordmgr.patch

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -346,6 +346,13 @@ var gBrowserInit = {
       )?.removeAttribute("key");
     }
 
+    if (Services.prefs.getBoolPref("librewolf.hidePasswdmgr", false)) {
+      PanelMultiView.getViewNode(
+        document,
+        "appMenu-passwords-button"
+      )?.remove();
+    }
+
     this._loadHandled = true;
   },
 
