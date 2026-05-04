95015f6c0217f45ad5e97866bb73e3629c571b66
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:46:06 -0500

    Bug 1932600 - Simplify chrome attribute handling. r=smaug,dao 2024-11-28 20:12:51 2e5c43e2e75be1c2bf55eb2f801dde0400989e31 Bug 1932600 - Use customtitlebar rather than tabsintitlebar in front-end code. r=dao,desktop-theme-reviewers,tabbrowser-reviewers,sidebar-reviewers,nsharpley 2024-11-28 20:12:51 768894a5121a32617f6557e8ffcd73e5d60c1f8a Bug 1930292 - Turn chromemargin into a boolean attribute. r=win-reviewers,desktop-theme-reviewers,pip-reviewers,firefox-desktop-core-reviewers ,dao,mconley,rkraesig 2024-11-28 20:12:50 feebe3d11d0df05cfdb0fc498a266ffe138e6386 Bug 1930292 - Don't extend into the frame border area with chromemargin=0,0,0,0. r=win-reviewers,rkraesig 2024-11-28 20:12:50 b04cbfd57830e1760f56b1106daef66a7e32f1d3 Bug 1930292 - Stop setting non-zero chromemargin values. r=win-reviewers,handyman,firefox-desktop-core-reviewers ,dao 2024-11-28 20:12:49 dcf6dc5521696e3b02071f162ede0af2b460fc85

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -113,13 +113,12 @@ var gBrowserInit = {
       }
     }
 
-    // Run menubar initialization first, to avoid CustomTitlebar code picking
+    // Run menubar initialization first, to avoid TabsInTitlebar code picking
     // up mutations from it and causing a reflow.
     AutoHideMenubar.init();
-    // Update the customtitlebar attribute so the window can be sized
-    // correctly.
+    // Update the chromemargin attribute so the window can be sized correctly.
     window.TabBarVisibility.update();
-    CustomTitlebar.init();
+    TabsInTitlebar.init();
 
     new LightweightThemeConsumer(document);
 
@@ -991,7 +990,7 @@ var gBrowserInit = {
   onUnload() {
     gUIDensity.uninit();
 
-    CustomTitlebar.uninit();
+    TabsInTitlebar.uninit();
 
     ToolbarIconColor.uninit(window);
 
