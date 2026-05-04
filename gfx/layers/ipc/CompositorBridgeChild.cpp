6b3393a6b9408838fd1a801d11c3d7615f199664
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:46:37 -0500

    Bug 1922717- Fix CompositorBridgeChild::SendFlushRenderingAsync() as to send async IPC r=gfx-reviewers,bradwerth 2024-10-04 14:09:44 5367802d82c74a5d62f05945039e3910fd20ed27

--- gfx/layers/ipc/CompositorBridgeChild.cpp
+++ gfx/layers/ipc/CompositorBridgeChild.cpp
@@ -363,7 +363,7 @@ bool CompositorBridgeChild::SendFlushRenderingAsync(
   if (!mCanSend) {
     return false;
   }
-  return PCompositorBridgeChild::SendFlushRenderingAsync(aReasons);
+  return PCompositorBridgeChild::SendFlushRendering(aReasons);
 }
 
 void CompositorBridgeChild::SetForceSyncFlushRendering(
