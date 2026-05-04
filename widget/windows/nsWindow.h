78ad17550c7d337781585fc5bd112ad8bbfda258
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:51:37 -0500

    Bug 1845746 - Stop using DwmCompositionEnabled in widget/windows/. r=handyman 2023-08-11 02:58:03 e897957107a0014c5f7734d13ba9577a3e5129ee

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -484,6 +484,8 @@ class nsWindow final : public nsBaseWidget,
   static LRESULT CALLBACK WindowProcInternal(HWND hWnd, UINT msg, WPARAM wParam,
                                              LPARAM lParam);
 
+  static BOOL CALLBACK BroadcastMsgToChildren(HWND aWnd, LPARAM aMsg);
+  static BOOL CALLBACK BroadcastMsg(HWND aTopWindow, LPARAM aMsg);
   static BOOL CALLBACK DispatchStarvedPaints(HWND aTopWindow, LPARAM aMsg);
   static BOOL CALLBACK RegisterTouchForDescendants(HWND aTopWindow,
                                                    LPARAM aMsg);
@@ -514,6 +516,7 @@ class nsWindow final : public nsBaseWidget,
   void UpdateGetWindowInfoCaptionStatus(bool aActiveCaption);
   void ResetLayout();
   void InvalidateNonClientRegion();
+  HRGN ExcludeNonClientFromPaintRegion(HRGN aRegion);
   static const wchar_t* GetMainWindowClass();
   HWND GetOwnerWnd() const { return ::GetWindow(mWnd, GW_OWNER); }
   bool IsOwnerForegroundWindow() const {
