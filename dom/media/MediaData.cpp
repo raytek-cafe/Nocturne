f3c6fb2666e418eaa71e04fd489eb858028f87da
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:52:45 -0500

    Bug 1844908 - Remove pre-Win10-specific codepath from browser/app/winlauncher/. r=mhowell 2023-07-26 16:52:50 2c10acccb65384cf135235ef17eb8a13177d3244 Bug 1844908 - Remove pre-Win10-specific codepath from browser/components/shell/. r=nalexander 2023-07-26 16:52:50 f90f9c3b98be14f497a770b2e5cd2ab0e386b902 Bug 1844908 - Remove pre-Win10-specific codepath from dom/media/. r=alwu 2023-07-26 16:52:50 37deadb8c291950ef657b6b79154d53a8a1d2a00 Bug 1844908 - Remove pre-Win10-specific codepath from hal/. r=gsvelto 2023-07-26 16:52:51 04ae2f4d00cd399fef5c200d621af6ec8e175012 Bug 1844908 - Remove pre-Win10-specific codepath from ipc/. r=ipc-reviewers,nika 2023-07-26 16:52:51 51e64d1ad0a11dc09be54a6fadee72d9a220190e Bug 1844908 - Remove pre-Win10-specific codepath from memory/. r=glandium 2023-07-26 16:52:51 0de8fc9e73d416ef30e03f7026161c26126bb943 Bug 1844908 - Remove pre-Win10-specific codepath from mozglue/. r=glandium 2023-07-26 16:52:52 e07ecec68d6c196afd564a45a8ef116655703f56 Bug 1844908 - Remove pre-Win10-specific codepath from security/manager/. r=keeler 2023-07-26 16:52:52 c9902c9701c7817f5fa280fea5891ef484105241 Bug 1844908 - Remove pre-Win10-specific codepath from security/sandbox/win/. r=bobowen 2023-07-26 16:52:52 d15bcc1b5ad4ad312608fd91d0518b88e8b421af Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/components/parentalcontrols/. r=handyman 2023-07-26 16:52:52 57fca85ef341aba59865a09c9871270e19ba62aa Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/crashreporter/. r=gsvelto 2023-07-26 16:52:52 e33acd085807d91e2ed3c29b1779a7be11b009fc Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/mozapps/defaultagent/. r=nrishel 2023-07-26 16:52:53 0167630b2ef198a0bee71bd9eac8cd4c042ee3e7 Bug 1844908 - Remove pre-Win10-specific codepath from toolkit/system/windowsPackageManager/. r=bhearsum,nrishel 2023-07-26 16:52:53 7f1ae83c4d6315412308c94e8072bc9c43a9a21a Bug 1844908 - Remove pre-Win10-specific codepath from DLL Blocklist Services. r=gstoll 2023-07-26 16:52:53 fd0eb6df78812bb3428105711cca916a5b223344 Bug 1844908 - Remove pre-Win10-specific codepath from Gecko Profiler. r=mstange 2023-07-26 16:52:53 578ce00f5f3a1ab9f99d969b59b049e2fb67d405 Bug 1844908 - Remove pre-Win10-specific codepath from uriloader/exthandler/. r=Gijs 2023-07-26 16:52:53 fa4242adbd52dd4ab8e6525a6a21d516b5ec5d23 Bug 1844908 - Remove pre-Win10-specific codepath from widget/. r=handyman 2023-07-26 16:52:54 a3e419385eba10cf518f6d6baaca4901162bf4c5 Bug 1844908 - Remove pre-Win10-specific codepath from xpcom/. r=emilio 2023-07-26 16:52:54 5362a280949316ba0451a99a9fc26248d61144f6

--- dom/media/MediaData.cpp
+++ dom/media/MediaData.cpp
@@ -21,6 +21,7 @@
 #include <stdint.h>
 
 #ifdef XP_WIN
+#  include "mozilla/WindowsVersion.h"
 #  include "mozilla/gfx/DeviceManagerDx.h"
 #  include "mozilla/layers/D3D11ShareHandleImage.h"
 #  include "mozilla/layers/D3D11YCbCrImage.h"
@@ -447,9 +448,11 @@ Result<already_AddRefed<VideoData>, MediaResult> VideoData::CreateAndCopyData(
     }
   }
 
+  // We disable this code path on Windows version earlier of Windows 8 due to
+  // intermittent crashes with old drivers. See bug 1405110.
   // D3D11YCbCrImage can only handle YCbCr images using 3 non-interleaved planes
   // non-zero mSkip value indicates that one of the plane would be interleaved.
-  if (XRE_IsGPUProcess() && aAllocator && aAllocator->SupportsD3D11() &&
+  if (IsWin8OrLater() && XRE_IsGPUProcess() && aAllocator && aAllocator->SupportsD3D11() &&
       !ConvertToNV12AtImageHost(aBuffer, aPicture, aAllocator) &&
       aBuffer.mPlanes[0].mSkip == 0 && aBuffer.mPlanes[1].mSkip == 0 &&
       aBuffer.mPlanes[2].mSkip == 0) {
