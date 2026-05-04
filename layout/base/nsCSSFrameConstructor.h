611a467f51963b8f87ca203c964f60f6a25c56c0
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:48:54 -0500

    Bug 1362907 - Make select use nsHTMLButtonControlFrame for layout. r=jfkthame,dholbert 2024-02-29 19:15:52 14c69b26721a27c6db51a28a1e4f22e4603e6aea

--- layout/base/nsCSSFrameConstructor.h
+++ layout/base/nsCSSFrameConstructor.h
@@ -1380,6 +1380,14 @@ class nsCSSFrameConstructor final : public nsFrameManager {
                                              nsFrameState aTypeBit);
 
  private:
+  // ConstructSelectFrame puts the new frame in aFrameList and
+  // handles the kids of the select.
+  nsIFrame* ConstructSelectFrame(nsFrameConstructorState& aState,
+                                 FrameConstructionItem& aItem,
+                                 nsContainerFrame* aParentFrame,
+                                 const nsStyleDisplay* aStyleDisplay,
+                                 nsFrameList& aFrameList);
+
   // ConstructFieldSetFrame puts the new frame in aFrameList and
   // handles the kids of the fieldset
   nsIFrame* ConstructFieldSetFrame(nsFrameConstructorState& aState,
@@ -1387,12 +1395,6 @@ class nsCSSFrameConstructor final : public nsFrameManager {
                                    nsContainerFrame* aParentFrame,
                                    const nsStyleDisplay* aStyleDisplay,
                                    nsFrameList& aFrameList);
-
-  nsIFrame* ConstructListBoxSelectFrame(nsFrameConstructorState& aState,
-                                        FrameConstructionItem& aItem,
-                                        nsContainerFrame* aParentFrame,
-                                        const nsStyleDisplay* aStyleDisplay,
-                                        nsFrameList& aFrameList);
 
   // Creates a block frame wrapping an anonymous ruby frame.
   nsIFrame* ConstructBlockRubyFrame(nsFrameConstructorState& aState,
@@ -1430,8 +1432,6 @@ class nsCSSFrameConstructor final : public nsFrameManager {
                                                    nsIFrame* aParentFrame,
                                                    ComputedStyle&);
   // HTML data-finding helper functions
-  static const FrameConstructionData* FindSelectData(const Element&,
-                                                     ComputedStyle&);
   static const FrameConstructionData* FindImgData(const Element&,
                                                   ComputedStyle&);
   static const FrameConstructionData* FindGeneratedImageData(const Element&,
