45179d241f8432f07f1934c42ecaf4362caa58fc
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:51:00 -0500

    Bug 1856104 - Remove useless version checks from JavaScript code. r=Gijs,webcompat-reviewers,settings-reviewers,search-reviewers,Standard8,twisniewski 2023-10-03 03:00:07 8262afb8c6e4bade9230df876e8c5e8cb8cc3eaf

--- browser/components/uitour/UITour.sys.mjs
+++ browser/components/uitour/UITour.sys.mjs
@@ -1816,7 +1816,7 @@ export var UITour = {
 
       let canSetDefaultBrowserInBackground = true;
       if (
-        AppConstants.platform == "win" ||
+        AppConstants.isPlatformAndVersionAtLeast("win", "6.2") ||
         AppConstants.isPlatformAndVersionAtLeast("macosx", "10.10")
       ) {
         canSetDefaultBrowserInBackground = false;
