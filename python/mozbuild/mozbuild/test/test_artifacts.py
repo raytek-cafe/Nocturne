7a2b86ef75d2d1455c95343dcc912e1b49e860b8
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:42:05 -0500

    Bug 1966470 - Simplify how Thunderbird artifacts get found. r=nalexander 2025-05-23 06:32:51 2d554b7365cbcf26f13cd17d50b0fbeaabc0b04b Bug 1966470 - Part 2: Use `mach artifact install --unfiltered-project-package` in single-locale repacks. r=firefox-build-system-reviewers,glandium 2025-05-22 00:32:56 52347eb1347506b2d883cfcd0b59b25d656da0cd Bug 1966470 - Part 1: Add `mach artifact install --unfiltered-project-package` command. r=firefox-build-system-reviewers,glandium 2025-05-22 00:32:56 a8d08169d9fd305d40783410b2960cd7816145d0 Bug 1966470 - Pre: Make `l10n-repack.py` command line easier to copy-paste. r=firefox-build-system-reviewers,glandium 2025-05-22 00:32:56 f4d2d352e2382ac35d3a950f8692be4abebc815a

--- python/mozbuild/mozbuild/test/test_artifacts.py
+++ python/mozbuild/mozbuild/test/test_artifacts.py
@@ -7,16 +7,11 @@ from unittest import TestCase, mock
 import buildconfig
 import mozunit
 
-from mozbuild.artifacts import (
-    ArtifactJob,
-    GeckoJobConfiguration,
-    ThunderbirdJobConfiguration,
-)
+from mozbuild.artifacts import ArtifactJob, ThunderbirdMixin
 
 
 class FakeArtifactJob(ArtifactJob):
     package_re = r""
-    job_configuration = GeckoJobConfiguration
 
 
 class TestArtifactJob(TestCase):
@@ -30,43 +25,38 @@ class TestArtifactJob(TestCase):
 
     def test_candidate_trees_with_empty_file(self):
         self._assert_candidate_trees(
-            version_display="",
-            expected_trees=GeckoJobConfiguration.default_candidate_trees,
+            version_display="", expected_trees=ArtifactJob.default_candidate_trees
         )
 
     def test_candidate_trees_with_beta_version(self):
         self._assert_candidate_trees(
-            version_display="92.1b2",
-            expected_trees=GeckoJobConfiguration.beta_candidate_trees,
+            version_display="92.1b2", expected_trees=ArtifactJob.beta_candidate_trees
         )
 
     def test_candidate_trees_with_esr_version(self):
         self._assert_candidate_trees(
-            version_display="91.3.0esr",
-            expected_trees=GeckoJobConfiguration.esr_candidate_trees,
+            version_display="91.3.0esr", expected_trees=ArtifactJob.esr_candidate_trees
         )
 
     def test_candidate_trees_with_nightly_version(self):
         self._assert_candidate_trees(
-            version_display="95.0a1",
-            expected_trees=GeckoJobConfiguration.nightly_candidate_trees,
+            version_display="95.0a1", expected_trees=ArtifactJob.nightly_candidate_trees
         )
 
     def test_candidate_trees_with_release_version(self):
         self._assert_candidate_trees(
-            version_display="93.0.1",
-            expected_trees=GeckoJobConfiguration.default_candidate_trees,
+            version_display="93.0.1", expected_trees=ArtifactJob.default_candidate_trees
         )
 
     def test_candidate_trees_with_newline_before_version(self):
         self._assert_candidate_trees(
             version_display="\n\n91.3.0esr",
-            expected_trees=GeckoJobConfiguration.esr_candidate_trees,
+            expected_trees=ArtifactJob.esr_candidate_trees,
         )
 
     def test_property_is_cached(self):
         job = FakeArtifactJob()
-        expected_trees = GeckoJobConfiguration.esr_candidate_trees
+        expected_trees = ArtifactJob.esr_candidate_trees
 
         buildconfig.substs["MOZ_APP_VERSION_DISPLAY"] = "91.3.0.esr"
         self.assertEqual(job.candidate_trees, expected_trees)
@@ -74,6 +64,10 @@ class TestArtifactJob(TestCase):
         # `MOZ_APP_VERSION_DISPLAY` won't have any impact.
         buildconfig.substs["MOZ_APP_VERSION_DISPLAY"] = ""
         self.assertEqual(job.candidate_trees, expected_trees)
+
+
+class FakeThunderbirdJob(ThunderbirdMixin, FakeArtifactJob):
+    pass
 
 
 class TestThunderbirdMixin(TestCase):
@@ -86,9 +80,7 @@ class TestThunderbirdMixin(TestCase):
                 "MOZ_SOURCE_REPO": source_repo,
             },
         ):
-            job = FakeArtifactJob(
-                override_job_configuration=ThunderbirdJobConfiguration
-            )
+            job = FakeThunderbirdJob()
             self.assertGreater(len(job.candidate_trees), 0)
             self.assertEqual(job.candidate_trees, expected_trees)
 
@@ -96,33 +88,33 @@ class TestThunderbirdMixin(TestCase):
         self._assert_candidate_trees(
             version_display="92.1b2",
             source_repo="https://hg.mozilla.org/releases/comm-beta",
-            expected_trees=ThunderbirdJobConfiguration.beta_candidate_trees,
+            expected_trees=ThunderbirdMixin.beta_candidate_trees,
         )
 
     def test_candidate_trees_with_esr_version(self):
         self._assert_candidate_trees(
             version_display="91.3.0esr",
             source_repo="https://hg.mozilla.org/releases/comm-esr91",
-            expected_trees=ThunderbirdJobConfiguration.esr_candidate_trees,
+            expected_trees=ThunderbirdMixin.esr_candidate_trees,
         )
 
     def test_candidate_trees_with_nightly_version(self):
         self._assert_candidate_trees(
             version_display="95.0a1",
             source_repo="https://hg.mozilla.org/comm-central",
-            expected_trees=ThunderbirdJobConfiguration.nightly_candidate_trees,
+            expected_trees=ThunderbirdMixin.nightly_candidate_trees,
         )
 
     def test_candidate_trees_with_release_version(self):
         self._assert_candidate_trees(
             version_display="93.0.1",
             source_repo="https://hg.mozilla.org/releases/comm-release",
-            expected_trees=ThunderbirdJobConfiguration.default_candidate_trees,
+            expected_trees=ThunderbirdMixin.default_candidate_trees,
         )
 
     def test_property_is_cached(self):
-        job = FakeArtifactJob(override_job_configuration=ThunderbirdJobConfiguration)
-        expected_trees = ThunderbirdJobConfiguration.esr_candidate_trees
+        job = FakeThunderbirdJob()
+        expected_trees = ThunderbirdMixin.esr_candidate_trees
 
         buildconfig.substs["MOZ_APP_VERSION_DISPLAY"] = "91.3.0.esr"
         self.assertEqual(job.candidate_trees, expected_trees)
