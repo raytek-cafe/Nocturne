d1afff52dbebc028de0bd7b84ff4bab696ebb865
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:54:35 -0500

    Bug 1756474 - Add locking to gfxPlatformFontList and gfxFontFamily, to allow use from worker threads. r=lsalzman 2022-04-19 22:20:21 7bf4d76a17cb009ff6b0ddfee4fd03e6073a9a1d

--- gfx/thebes/gfxFontEntry.h
+++ gfx/thebes/gfxFontEntry.h
@@ -1061,7 +1061,6 @@ class gfxFontFamily {
 
   // mark this family as being in the "bad" underline offset blocklist
   void SetBadUnderlineFamily() {
-    mozilla::AutoWriteLock lock(mLock);
     mIsBadUnderlineFamily = true;
     if (mHasStyles) {
       SetBadUnderlineFonts();
