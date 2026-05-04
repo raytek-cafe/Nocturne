e166d7e4c09b855682f21abc820760f66772a346
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:52:03 -0500

    Bug 1846782 - Fix nsNativeTheme*::GetWidget{Border,Padding} for always-non-native widgets. r=aminomancer 2023-08-03 04:30:21 47296916c60d506c8bd2cddb50a54f9af24012e7

--- widget/windows/nsNativeThemeWin.cpp
+++ widget/windows/nsNativeThemeWin.cpp
@@ -1033,10 +1033,6 @@ static void ScaleForFrameDPI(LayoutDeviceIntSize* aSize, nsIFrame* aFrame) {
 
 LayoutDeviceIntMargin nsNativeThemeWin::GetWidgetBorder(
     nsDeviceContext* aContext, nsIFrame* aFrame, StyleAppearance aAppearance) {
-  if (IsWidgetAlwaysNonNative(aFrame, aAppearance)) {
-    return Theme::GetWidgetBorder(aContext, aFrame, aAppearance);
-  }
-
   LayoutDeviceIntMargin result;
   mozilla::Maybe<nsUXThemeClass> themeClass = GetThemeClass(aAppearance);
   HTHEME theme = NULL;
@@ -1090,7 +1086,7 @@ bool nsNativeThemeWin::GetWidgetPadding(nsDeviceContext* aContext,
                                         nsIFrame* aFrame,
                                         StyleAppearance aAppearance,
                                         LayoutDeviceIntMargin* aResult) {
-  if (IsWidgetAlwaysNonNative(aFrame, aAppearance)) {
+  if (IsWidgetNonNative(aFrame, aAppearance) == NonNative::Always) {
     return Theme::GetWidgetPadding(aContext, aFrame, aAppearance, aResult);
   }
 
