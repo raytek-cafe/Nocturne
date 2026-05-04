8b402262359ab12192f1c68ce633dce8efe3d864
the_r3dacted <k4sum1@eclipse.cx>
2026-05-04 07:53:05 -0500

    Bug 1838460 - Remove unused -moz-buttondefault color. r=stransky 2023-07-19 01:21:40 08e09f629974294f07f6b593738a2c9e00f991a4

--- servo/components/style/values/specified/color.rs
+++ servo/components/style/values/specified/color.rs
@@ -198,6 +198,7 @@ pub enum SystemColor {
     Window,
     Windowframe,
     Windowtext,
+    MozButtondefault,
     #[parse(aliases = "-moz-default-color")]
     Canvastext,
     #[parse(aliases = "-moz-default-background-color")]
