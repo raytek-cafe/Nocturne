6a0f839f186a452c819bc19ee2190151b78dc163
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-05-17 11:34:49 -0500

    Disable DComp on 10 when faking older Windows version for theme
    Aero style massively breaks if DComp is used, so disable it here.
    
    Based on https://github.com/ImSwordQueen/Nocturne/commit/31871e0f211d1d6fe03a5f0ab5089f2a01d9012c

--- gfx/config/gfxConfigManager.cpp
+++ gfx/config/gfxConfigManager.cpp
@@ -8,6 +8,7 @@
 #include "mozilla/Components.h"
 #include "mozilla/StaticPrefs_gfx.h"
 #include "mozilla/StaticPrefs_layers.h"
+#include "mozilla/StaticPrefs_widget.h"
 #include "gfxConfig.h"
 #include "gfxPlatform.h"
 #include "nsIGfxInfo.h"
@@ -56,7 +57,9 @@ void gfxConfigManager::Init() {
 #ifdef XP_WIN
   DeviceManagerDx::Get()->CheckHardwareStretchingSupport(mHwStretchingSupport);
   mScaledResolution = HasScaledResolution();
-  mIsWin10OrLater = IsWin10OrLater();
+  // Nocturne: Only treat as Win10+ for DComp when actually on Win10+ and not spoofed as an older version.
+  int overrideWinVer = StaticPrefs::widget_native_controls_override_win_version();
+  mIsWin10OrLater = IsWin10OrLater() && (overrideWinVer == 0 || overrideWinVer == 10);
   mWrCompositorDCompRequired = true;
 #else
   ++mHwStretchingSupport.mBoth;
