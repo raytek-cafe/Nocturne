270db7bca24672a55ccab1a1108f78056143c2b2
the_r3dacted <k4sum1@eclipse.cx>
2026-05-21 11:32:38 -0500

    Allow all pdf files to be printed
    Revert "Bug 1978985 - Disable printing when enablePermission is true and the pdf isn't allowed to be printed  a=RyanVM"
    
    This reverts commit 90bc3d36c3d0465648bc7ce1092756b6965b24a0.

--- toolkit/components/pdfjs/content/web/viewer-geckoview.mjs
+++ toolkit/components/pdfjs/content/web/viewer-geckoview.mjs
@@ -7044,7 +7044,6 @@ class PDFViewer {
   #eventAbortController = null;
   #minDurationToUpdateCanvas = 0;
   #mlManager = null;
-  #printingAllowed = true;
   #scrollTimeoutId = null;
   #switchAnnotationEditorModeAC = null;
   #switchAnnotationEditorModeTimeoutId = null;
@@ -7122,9 +7121,6 @@ class PDFViewer {
         pdfPage?.cleanup();
       }
     });
-  }
-  get printingAllowed() {
-    return this.#printingAllowed;
   }
   get pagesCount() {
     return this._pages.length;
@@ -7305,18 +7301,8 @@ class PDFViewer {
       textLayerMode: this.#textLayerMode
     };
     if (!permissions) {
-      this.#printingAllowed = true;
-      this.eventBus.dispatch("printingallowed", {
-        source: this,
-        isAllowed: this.#printingAllowed
-      });
       return params;
     }
-    this.#printingAllowed = permissions.includes(PermissionFlag.PRINT_HIGH_QUALITY) || permissions.includes(PermissionFlag.PRINT);
-    this.eventBus.dispatch("printingallowed", {
-      source: this,
-      isAllowed: this.#printingAllowed
-    });
     if (!permissions.includes(PermissionFlag.COPY) && this.#textLayerMode === TextLayerMode.ENABLE) {
       params.textLayerMode = TextLayerMode.ENABLE_PERMISSIONS;
     }
@@ -7414,7 +7400,6 @@ class PDFViewer {
       this._scriptingManager?.setDocument(null);
       this.#annotationEditorUIManager?.destroy();
       this.#annotationEditorUIManager = null;
-      this.#printingAllowed = true;
     }
     this.pdfDocument = pdfDocument;
     if (!pdfDocument) {
@@ -8817,7 +8802,6 @@ const PDFViewerApplication = {
   _caretBrowsing: null,
   _isScrolling: false,
   editorUndoBar: null,
-  _printPermissionPromise: null,
   async initialize(appConfig) {
     this.appConfig = appConfig;
     try {
@@ -9165,16 +9149,9 @@ const PDFViewerApplication = {
         console.warn(msg);
       });
     }
-    const togglePrintingButtons = visible => {
-      appConfig.toolbar?.print?.classList.toggle("hidden", !visible);
-      appConfig.secondaryToolbar?.printButton.classList.toggle("hidden", !visible);
-    };
     if (!this.supportsPrinting) {
-      togglePrintingButtons(false);
-    } else {
-      eventBus.on("printingallowed", ({
-        isAllowed
-      }) => togglePrintingButtons(isAllowed));
+      appConfig.toolbar?.print?.classList.add("hidden");
+      appConfig.secondaryToolbar?.printButton.classList.add("hidden");
     }
     if (!this.supportsFullscreen) {
       appConfig.secondaryToolbar?.presentationModeButton.classList.add("hidden");
@@ -9488,20 +9465,6 @@ const PDFViewerApplication = {
   },
   load(pdfDocument) {
     this.pdfDocument = pdfDocument;
-    this._printPermissionPromise = new Promise(resolve => {
-      this.eventBus.on("printingallowed", ({
-        isAllowed
-      }) => {
-        if (!isAllowed) {
-          window.print = () => {
-            console.warn("Printing is not allowed.");
-          };
-        }
-        resolve(isAllowed);
-      }, {
-        once: true
-      });
-    });
     pdfDocument.getDownloadInfo().then(({
       length
     }) => {
@@ -9855,7 +9818,7 @@ const PDFViewerApplication = {
     if (this.printService) {
       return;
     }
-    if (!this.supportsPrinting || !this.pdfViewer.printingAllowed) {
+    if (!this.supportsPrinting) {
       this._otherError("pdfjs-printing-not-supported");
       return;
     }
@@ -9906,8 +9869,8 @@ const PDFViewerApplication = {
   requestPresentationMode() {
     this.pdfPresentationMode?.request();
   },
-  async triggerPrinting() {
-    if (this.supportsPrinting && (await this._printPermissionPromise)) {
+  triggerPrinting() {
+    if (this.supportsPrinting) {
       window.print();
     }
   },
--- toolkit/components/pdfjs/content/web/viewer.mjs
+++ toolkit/components/pdfjs/content/web/viewer.mjs
@@ -10232,7 +10232,6 @@ class PDFViewer {
   #eventAbortController = null;
   #minDurationToUpdateCanvas = 0;
   #mlManager = null;
-  #printingAllowed = true;
   #scrollTimeoutId = null;
   #switchAnnotationEditorModeAC = null;
   #switchAnnotationEditorModeTimeoutId = null;
@@ -10310,9 +10309,6 @@ class PDFViewer {
         pdfPage?.cleanup();
       }
     });
-  }
-  get printingAllowed() {
-    return this.#printingAllowed;
   }
   get pagesCount() {
     return this._pages.length;
@@ -10493,18 +10489,8 @@ class PDFViewer {
       textLayerMode: this.#textLayerMode
     };
     if (!permissions) {
-      this.#printingAllowed = true;
-      this.eventBus.dispatch("printingallowed", {
-        source: this,
-        isAllowed: this.#printingAllowed,
-      });
       return params;
     }
-    this.#printingAllowed = permissions.includes(PermissionFlag.PRINT_HIGH_QUALITY) || permissions.includes(PermissionFlag.PRINT);
-    this.eventBus.dispatch("printingallowed", {
-      source: this,
-      isAllowed: this.#printingAllowed
-    });
     if (!permissions.includes(PermissionFlag.COPY) && this.#textLayerMode === TextLayerMode.ENABLE) {
       params.textLayerMode = TextLayerMode.ENABLE_PERMISSIONS;
     }
@@ -10602,7 +10588,6 @@ class PDFViewer {
       this._scriptingManager?.setDocument(null);
       this.#annotationEditorUIManager?.destroy();
       this.#annotationEditorUIManager = null;
-      this.#printingAllowed = true;
     }
     this.pdfDocument = pdfDocument;
     if (!pdfDocument) {
@@ -13425,7 +13410,6 @@ const PDFViewerApplication = {
   _caretBrowsing: null,
   _isScrolling: false,
   editorUndoBar: null,
-  _printPermissionPromise: null,
   async initialize(appConfig) {
     this.appConfig = appConfig;
     try {
@@ -13772,16 +13756,9 @@ const PDFViewerApplication = {
         console.warn(msg);
       });
     }
-    const togglePrintingButtons = visible => {
-      appConfig.toolbar?.print?.classList.toggle("hidden", !visible);
-      appConfig.secondaryToolbar?.printButton.classList.toggle("hidden", !visible);
-    };
     if (!this.supportsPrinting) {
-      togglePrintingButtons(false);
-    } else {
-      eventBus.on("printingallowed", ({
-        isAllowed
-      }) => togglePrintingButtons(isAllowed));
+      appConfig.toolbar?.print?.classList.add("hidden");
+      appConfig.secondaryToolbar?.printButton.classList.add("hidden");
     }
     if (!this.supportsFullscreen) {
       appConfig.secondaryToolbar?.presentationModeButton.classList.add("hidden");
@@ -14095,20 +14072,6 @@ const PDFViewerApplication = {
   },
   load(pdfDocument) {
     this.pdfDocument = pdfDocument;
-    this._printPermissionPromise = new Promise(resolve => {
-      this.eventBus.on("printingallowed", ({
-        isAllowed
-      }) => {
-        if (!isAllowed) {
-          window.print = () => {
-            console.warn("Printing is not allowed.");
-          };
-        }
-        resolve(isAllowed);
-      }, {
-        once: true
-      });
-    });
     pdfDocument.getDownloadInfo().then(({
       length
     }) => {
@@ -14495,7 +14458,7 @@ const PDFViewerApplication = {
     if (this.printService) {
       return;
     }
-    if (!this.supportsPrinting || !this.pdfViewer.printingAllowed) {
+    if (!this.supportsPrinting) {
       this._otherError("pdfjs-printing-not-supported");
       return;
     }
@@ -14546,8 +14509,8 @@ const PDFViewerApplication = {
   requestPresentationMode() {
     this.pdfPresentationMode?.request();
   },
-  async triggerPrinting() {
-    if (this.supportsPrinting && (await this._printPermissionPromise)) {
+  triggerPrinting() {
+    if (this.supportsPrinting) {
       window.print();
     }
   },
