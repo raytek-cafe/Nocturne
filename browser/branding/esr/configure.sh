957ec8591458924da66a8b1cbc10b72b2b4dd8a8
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-05-25 00:11:26 +0200

    Branding & rename: r3dfox to Nocturne
    Rename all r3dfox branding references to Nocturne.
    Update configure.sh, preferences, URLs, icons, manifests,
    and all other branding assets across esr/nightly/official channels.
    Includes git file renames (r3dfox.* to nocturne.*) and
    preference namespace changes (r3dfox.* to nocturne.*).

--- browser/branding/esr/configure.sh
+++ browser/branding/esr/configure.sh
@@ -2,20 +2,20 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_NAME=r3dfox_esr
+MOZ_APP_NAME=nocturne
 
-MOZ_APP_BASENAME="r3dfox ESR"
+MOZ_APP_BASENAME=Nocturne
 
-MOZ_APP_DISPLAYNAME="r3dfox ESR"
+MOZ_APP_DISPLAYNAME=Nocturne
 
-MOZ_APP_REMOTINGNAME=r3dfox_esr
+MOZ_APP_REMOTINGNAME=com.nocturne.nocturne
 
-MOZ_MACBUNDLE_ID=com.eclipse.r3dfox_esr
+MOZ_MACBUNDLE_ID=com.nocturne.nocturne
 
-MOZ_DISTRIBUTION_ID=com.eclipse
+MOZ_DISTRIBUTION_ID=com.nocturne
 
-MOZ_MACBUNDLE_NAME="r3dfox.app"
+MOZ_MACBUNDLE_NAME="nocturne.app"
 
-MOZ_SOURCE_REPO=https://github.com/Eclipse-Community/r3dfox
+MOZ_SOURCE_REPO=https://github.com/raytek-cafe/Nocturne
 
 MOZ_DEVTOOLS=all
--- browser/branding/nightly/configure.sh
+++ browser/branding/nightly/configure.sh
@@ -2,20 +2,20 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_NAME=plasmafox
+MOZ_APP_NAME=nocturne
 
-MOZ_APP_BASENAME=Plasmafox
+MOZ_APP_BASENAME=Nocturne
 
-MOZ_APP_DISPLAYNAME=Plasmafox
+MOZ_APP_DISPLAYNAME=Nocturne
 
-MOZ_APP_REMOTINGNAME=plasmafox
+MOZ_APP_REMOTINGNAME=com.nocturne.nocturne
 
-MOZ_MACBUNDLE_ID=com.eclipse.plasmafox
+MOZ_MACBUNDLE_ID=com.nocturne.nocturne
 
-MOZ_DISTRIBUTION_ID=com.eclipse
+MOZ_DISTRIBUTION_ID=com.nocturne
 
-MOZ_MACBUNDLE_NAME="plasmafox.app"
+MOZ_MACBUNDLE_NAME="nocturne.app"
 
-MOZ_SOURCE_REPO=https://github.com/Eclipse-Community/r3dfox
+MOZ_SOURCE_REPO=https://github.com/raytek-cafe/Nocturne
 
 MOZ_DEVTOOLS=all
