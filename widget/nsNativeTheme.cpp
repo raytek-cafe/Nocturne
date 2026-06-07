692c5c3e550465839e107ce4d0154165a9342431
e3kskoy7wqk <e3kskoy7wqk@163.com>
2026-06-07 11:11:53 -0500

    Bug 1858349 - Remove now-unused menuarrow code. r=spohl,mac-reviewers,win-reviewers,rkraesig 2023-10-12 03:37:40 24fe7cd3ccc40f70e521f86975b2ab8e4d92277d

--- widget/nsNativeTheme.cpp
+++ widget/nsNativeTheme.cpp
@@ -509,6 +509,7 @@ bool nsNativeTheme::IsWidgetAlwaysNonNative(nsIFrame* aFrame,
                                             StyleAppearance aAppearance) {
   return IsWidgetScrollbarPart(aAppearance) ||
          aAppearance == StyleAppearance::FocusOutline ||
+         aAppearance == StyleAppearance::Menuarrow ||
          aAppearance == StyleAppearance::Toolbarbutton ||
          aAppearance == StyleAppearance::ProgressBar ||
          aAppearance == StyleAppearance::Meter ||
