41e74fb62258e65b71736e5c8f9fe3931b304fba
e3kskoy7wqk <e3kskoy7wqk@163.com>
2026-06-07 11:10:33 -0500

    Bug 1891063 - Re-introduce some of the opaque region handling removed in bug 1844241. r=win-reviewers,rkraesig 2024-04-23 07:17:51 b6f8a18455a4cb9f9ab24e5414051fff78e0a78f

--- widget/windows/nsWindow.h
+++ widget/windows/nsWindow.h
@@ -618,9 +618,6 @@ class nsWindow final : public nsIWidget {
   bool IsSimulatedClientArea(int32_t clientX, int32_t clientY);
   bool IsWindowButton(int32_t hitTestResult);
 
-  void UpdateOpaqueRegion(const LayoutDeviceIntRegion&) override;
-  void UpdateOpaqueRegionInternal();
-
   void SetColorScheme(const mozilla::Maybe<mozilla::ColorScheme>&) override;
   void SetMicaBackdrop(bool) override;
 
@@ -822,9 +819,6 @@ class nsWindow final : public nsIWidget {
 
   // Draggable titlebar region maintained by UpdateWindowDraggingRegion
   LayoutDeviceIntRegion mDraggableRegion;
-  // Opaque region maintained by UpdateOpaqueRegion (relative to the client
-  // area).
-  LayoutDeviceIntRegion mOpaqueRegion;
 
   LayoutDeviceIntRect mBounds;
 
@@ -835,6 +829,7 @@ class nsWindow final : public nsIWidget {
 
   // Transparency
   TransparencyMode mTransparencyMode = TransparencyMode::Opaque;
+  nsIntRegion mPossiblyTransparentRegion;
 
   // Win7 Gesture processing and management
   nsWinGesture mGesture;
