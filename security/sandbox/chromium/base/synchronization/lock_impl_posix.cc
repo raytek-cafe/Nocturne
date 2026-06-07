807c49469f8fe5e76eb289188bdbab47fbe713fe
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:16:47 -0500

    Inline LockImpl::Lock() and LockImpl::Try(). 2020-09-09 01:41:00 7d86528b57e35a9aa6c8ce34f143c971e667fe2e

--- security/sandbox/chromium/base/synchronization/lock_impl_posix.cc
+++ security/sandbox/chromium/base/synchronization/lock_impl_posix.cc
@@ -14,7 +14,6 @@
 #include "base/check_op.h"
 #include "base/posix/safe_strerror.h"
 #include "base/synchronization/lock.h"
-#include "base/synchronization/lock_metrics_recorder.h"
 #include "base/synchronization/synchronization_buildflags.h"
 #include "base/system/sys_info.h"
 #include "build/build_config.h"
@@ -131,8 +130,23 @@ LockImpl::~LockImpl() {
   DCHECK_EQ(rv, 0) << ". " << SystemErrorCodeToString(rv);
 }
 
-void LockImpl::LockInternal() {
-  LockMetricsRecorder::ScopedLockAcquisitionTimer timer;
+bool LockImpl::Try() {
+  int rv = pthread_mutex_trylock(&native_handle_);
+  DCHECK(rv == 0 || rv == EBUSY) << ". " << SystemErrorCodeToString(rv);
+  return rv == 0;
+}
+
+void LockImpl::Lock() {
+  // The ScopedLockAcquireActivity below is relatively expensive and so its
+  // actions can become significant due to the very large number of locks that
+  // tend to be used throughout the build. It is also not needed unless the lock
+  // is contended.
+  //
+  // To avoid this cost in the vast majority of the calls, simply "try" the lock
+  // first and only do the (tracked) blocking call if that fails. |Try()| is
+  // cheap on platforms with futex(), as it doesn't call into the kernel.
+  if (Try())
+    return;
   int rv = pthread_mutex_lock(&native_handle_);
   DCHECK_EQ(rv, 0) << ". " << SystemErrorCodeToString(rv);
 }
--- security/sandbox/chromium/base/synchronization/lock_impl_win.cc
+++ security/sandbox/chromium/base/synchronization/lock_impl_win.cc
@@ -6,8 +6,6 @@
 
 #include <windows.h>
 
-#include "base/synchronization/lock_metrics_recorder.h"
-
 namespace base {
 namespace internal {
 
@@ -15,8 +13,22 @@ LockImpl::LockImpl() : native_handle_(SRWLOCK_INIT) {}
 
 LockImpl::~LockImpl() = default;
 
-void LockImpl::LockInternal() {
-  LockMetricsRecorder::ScopedLockAcquisitionTimer timer;
+bool LockImpl::Try() {
+  return !!::TryAcquireSRWLockExclusive(
+      reinterpret_cast<PSRWLOCK>(&native_handle_));
+}
+
+void LockImpl::Lock() {
+  // The ScopedLockAcquireActivity below is relatively expensive and so its
+  // actions can become significant due to the very large number of locks that
+  // tend to be used throughout the build. It is also not needed unless the lock
+  // is contended.
+  //
+  // To avoid this cost in the vast majority of the calls, simply "try" the lock
+  // first and only do the (tracked) blocking call if that fails. |Try()| is
+  // cheap, as it doesn't call into the kernel.
+  if (Try())
+    return;
   ::AcquireSRWLockExclusive(reinterpret_cast<PSRWLOCK>(&native_handle_));
 }
 
