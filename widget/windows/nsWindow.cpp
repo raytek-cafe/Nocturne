c134b02f3534d25f4bcbf9000769fbbc7fd953be
e3kskoy7wqk <e3kskoy7wqk@163.com>
2026-06-07 11:10:54 -0500

    Bug 1878623 - Clean up some windows popup code. r=win-reviewers,rkraesig 2024-02-13 03:45:49 f549c33ac3619e571c9da8565d366ca474e189c9

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -1362,9 +1362,9 @@ static LPWSTR const gStockApplicationIcon = MAKEINTRESOURCEW(32512);
 const wchar_t* nsWindow::ChooseWindowClass(WindowType aWindowType) {
   switch (aWindowType) {
     case WindowType::Dialog:
-      return RegisterWindowClass(kClassNameDialog, 0, nullptr);
+      return RegisterWindowClass(kClassNameDialog, 0, 0);
     case WindowType::Popup:
-      return RegisterWindowClass(kClassNameDropShadow, 0,
+      return RegisterWindowClass(kClassNameDropShadow, CS_DROPSHADOW,
                                  gStockApplicationIcon);
     default:
       return RegisterWindowClass(GetMainWindowClass(), 0,
@@ -1669,6 +1669,15 @@ void nsWindow::Show(bool aState) {
 
   if (mWindowType == WindowType::Popup) {
     MOZ_ASSERT(ChooseWindowClass(mWindowType) == kClassNameDropShadow);
+    const bool shouldUseDropShadow =
+        mTransparencyMode != TransparencyMode::Transparent;
+
+    static bool sShadowEnabled = true;
+    if (sShadowEnabled != shouldUseDropShadow) {
+      ::SetClassLongA(mWnd, GCL_STYLE, shouldUseDropShadow ? CS_DROPSHADOW : 0);
+      sShadowEnabled = shouldUseDropShadow;
+    }
+
     // WS_EX_COMPOSITED conflicts with the WS_EX_LAYERED style and causes
     // some popup menus to become invisible.
     LONG_PTR exStyle = ::GetWindowLongPtrW(mWnd, GWL_EXSTYLE);
@@ -1798,6 +1807,26 @@ void nsWindow::Show(bool aState) {
 //
 // This does not take cloaking into account.
 bool nsWindow::IsVisible() const { return mIsVisible; }
+
+/**************************************************************
+ *
+ * SECTION: Window clipping utilities
+ *
+ * Used in Size and Move operations for setting the proper
+ * window clipping regions for window transparency.
+ *
+ **************************************************************/
+
+// XP and Vista visual styles sometimes require window clipping regions to be
+// applied for proper transparency. These routines are called on size and move
+// operations.
+// XXX this is apparently still needed in Windows 7 and later
+void nsWindow::ClearThemeRegion() {
+  if (mWindowType == WindowType::Popup && !IsPopupWithTitleBar() &&
+      (mPopupType == PopupType::Tooltip || mPopupType == PopupType::Panel)) {
+    SetWindowRgn(mWnd, nullptr, false);
+  }
+}
 
 /**************************************************************
  *
@@ -1975,6 +2004,8 @@ void nsWindow::Move(const DesktopPoint& aTopLeft) {
       }
     }
 #endif
+      ClearThemeRegion();
+
     UINT flags = SWP_NOZORDER | SWP_NOACTIVATE | SWP_NOSIZE;
     double oldScale = mDefaultScale;
     mResizeState = IN_SIZEMOVE;
@@ -2034,6 +2065,7 @@ void nsWindow::Resize(const DesktopSize& aSize, bool aRepaint) {
     if (!aRepaint) {
       flags |= SWP_NOREDRAW;
     }
+      ClearThemeRegion();
     double oldScale = mDefaultScale;
     mResizeState = RESIZING;
     VERIFY(::SetWindowPos(mWnd, nullptr, 0, 0, size.width, size.height, flags));
@@ -2105,6 +2137,8 @@ void nsWindow::Resize(const DesktopRect& aRect, bool aRepaint) {
     if (!aRepaint) {
       flags |= SWP_NOREDRAW;
     }
+
+      ClearThemeRegion();
 
     double oldScale = mDefaultScale;
     mResizeState = RESIZING;
