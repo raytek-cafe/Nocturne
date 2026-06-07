5c052bfa2a7eb4aedec8657aa77633d48dc94f20
e3kskoy7wqk <e3kskoy7wqk@163.com>
2026-06-07 11:06:21 -0500

    Bug 1959965 - Simplify nsWindow child set-up on Windows. r=win-reviewers,handyman 2025-04-15 03:00:16 424bee3ebebc09d952a0c3951bb0ef362b7d6ff0

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -815,9 +815,10 @@ static bool IsCloaked(HWND hwnd) {
  *
  **************************************************************/
 
-nsWindow::nsWindow()
+nsWindow::nsWindow(bool aIsChildWindow)
     : nsIWidget(BorderStyle::Default),
       mFrameState(std::in_place, this),
+      mIsChildWindow(aIsChildWindow),
       mMicaBackdrop(false),
       mLastPaintEndTime(TimeStamp::Now()),
       mCachedHitTestTime(TimeStamp::Now()) {
@@ -1459,7 +1460,7 @@ DWORD nsWindow::WindowStyle() {
       break;
 
     case WindowType::Popup:
-      style = WS_OVERLAPPED | WS_POPUP | WS_CLIPCHILDREN;
+      style = WS_OVERLAPPED | WS_POPUP;
       break;
 
     default:
@@ -1473,6 +1474,13 @@ DWORD nsWindow::WindowStyle() {
   }
 
   style &= ~WindowStylesRemovedForBorderStyle(mBorderStyle);
+
+  if (mIsChildWindow) {
+    style |= WS_CLIPCHILDREN;
+    if (!(style & WS_POPUP)) {
+      style |= WS_CHILD;  // WS_POPUP and WS_CHILD are mutually exclusive.
+    }
+  }
 
   VERIFY_WINDOW_STYLE(style);
   return style;
@@ -7044,6 +7052,14 @@ bool nsWindow::ShouldUseOffMainThreadCompositing() {
   if (mWindowType == WindowType::Popup && mPopupType == PopupType::Tooltip) {
     return false;
   }
+
+  // Content rendering of popup is always done by child window.
+  // See nsDocumentViewer::ShouldAttachToTopLevel().
+  if (mWindowType == WindowType::Popup && !mIsChildWindow) {
+    MOZ_ASSERT(!mParent);
+    return false;
+  }
+
   return nsIWidget::ShouldUseOffMainThreadCompositing();
 }
 
@@ -8403,7 +8419,7 @@ already_AddRefed<nsIWidget> nsIWidget::CreateTopLevelWindow() {
 }
 
 already_AddRefed<nsIWidget> nsIWidget::CreateChildWindow() {
-  nsCOMPtr<nsIWidget> window = new nsWindow();
+  nsCOMPtr<nsIWidget> window = new nsWindow(true);
   return window.forget();
 }
 
