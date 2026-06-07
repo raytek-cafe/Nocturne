59ac9ef366847f19e23ea7604d36f24c5480dbfa
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:13:47 -0500

    remove OnJobEmpty and OnProcessEmpty from sandbox policy 2023/3/30 9:57:04 0da61a42f2556375cc91c2cbd15d2a7e904b0569

--- security/sandbox/chromium/sandbox/win/src/sandbox_policy_base.cc
+++ security/sandbox/chromium/sandbox/win/src/sandbox_policy_base.cc
@@ -506,11 +506,7 @@ PolicyBase::PolicyBase(std::string_view tag)
       dispatcher_(nullptr),
       job_() {}
 
-PolicyBase::~PolicyBase() {
-  // Ensure this is cleared before other members - this terminates the process
-  // if it hasn't already finished.
-  target_.reset();
-}
+PolicyBase::~PolicyBase() {}
 
 TargetConfig* PolicyBase::GetConfig() {
   return config();
@@ -741,6 +737,18 @@ ResultCode PolicyBase::ApplyToTarget(std::unique_ptr<TargetProcess> target) {
 
   target_ = std::move(target);
   return SBOX_ALL_OK;
+}
+
+// Can only be called if a job was associated with this policy.
+bool PolicyBase::OnJobEmpty() {
+  target_.reset();
+  return true;
+}
+
+bool PolicyBase::OnProcessFinished(DWORD process_id) {
+  if (target_->ProcessId() == process_id)
+    target_.reset();
+  return true;
 }
 
 EvalResult PolicyBase::EvalPolicy(IpcTag service,
