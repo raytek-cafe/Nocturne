fc531c53e4bea135c268ab1840ea506db72177cb
ImSwordQueen <52015098+ImSwordQueen@users.noreply.github.com>
2026-06-09 08:02:09 -0500

    Disable Windows launch-on-login by default

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -323,7 +323,7 @@ pref("browser.startup.couldRestoreSession.count", 0);
 pref("browser.startup.preXulSkeletonUI", false);
 
 // Whether the checkbox to enable Windows launch on login is shown
-pref("browser.startup.windowsLaunchOnLogin.enabled", true);
+pref("browser.startup.windowsLaunchOnLogin.enabled", false);
 // Whether to show the launch on login infobar notification
 pref("browser.startup.windowsLaunchOnLogin.disableLaunchOnLoginPrompt", false);
 // Whether new installs default to launching Firefox on Windows login when the
