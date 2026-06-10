4b75dcb295d5c87bbc4ab354df40076b3061486d
OmegaAOL <hackersword666@gmail.com>
2026-06-10 05:53:05 -0700

    Port native prompt mode gating
    Ported and adapted from d126ce8fce68.

--- toolkit/components/prompts/src/Prompter.sys.mjs
+++ toolkit/components/prompts/src/Prompter.sys.mjs
@@ -981,9 +981,14 @@ class ModalPrompter {
       return;
     }
 
-    if (!ModalPrompter.tabModalEnabled &&
-      (modalType == MODAL_TYPE_TAB || modalType == MODAL_TYPE_CONTENT)) {
-      modalType = MODAL_TYPE_WINDOW;
+    if (
+      !ModalPrompter.tabModalEnabled &&
+      (modalType == MODAL_TYPE_TAB ||
+        modalType == MODAL_TYPE_CONTENT ||
+        modalType == MODAL_TYPE_INTERNAL_WINDOW)
+    ) {
+      this._modalType = MODAL_TYPE_WINDOW;
+      return;
     }
     // For content prompts for non-content windows, use window prompts:
     if (modalType == MODAL_TYPE_CONTENT && !this.browsingContext?.isContent) {
