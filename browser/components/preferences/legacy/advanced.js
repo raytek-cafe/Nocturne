7932ca05e6e4df97ec90970e5c0743cafde396f1
OmegaAOL <hackersword666@gmail.com>
2026-06-11 04:10:42 -0700

    Re-add Certificate Manager support

--- browser/components/preferences/legacy/advanced.js
+++ browser/components/preferences/legacy/advanced.js
@@ -796,7 +796,7 @@ var gAdvancedPane = {
   showCertificates: function ()
   {
     document.documentElement.openWindow("mozilla:certmanager",
-                                        "chrome://pippki/content/certManager.xul",
+                                        "chrome://pippki/content/certManager.xhtml",
                                         "", null);
   },
 
