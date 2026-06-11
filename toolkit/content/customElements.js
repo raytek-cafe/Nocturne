7e5aa8453f6ea567074c931a40ca4a9efcf46976
OmegaAOL <hackersword666@gmail.com>
2026-06-10 20:17:53 -0700

    Restore <textbox> in XUL as a thin wrapper around moz-input-box and the HTML text box

--- toolkit/content/customElements.js
+++ toolkit/content/customElements.js
@@ -916,6 +916,7 @@
       "chrome://global/content/elements/autocomplete-richlistitem.js",
       "chrome://global/content/elements/tabbox.js",
       "chrome://global/content/elements/text.js",
+	  "chrome://global/content/elements/textbox.js",
       "chrome://global/content/elements/toolbarbutton.js",
       "chrome://global/content/elements/tree.js",
       "chrome://global/content/elements/wizard.js",
