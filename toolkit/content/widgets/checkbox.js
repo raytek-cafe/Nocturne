2d171c8810fae355d75ddac8e222dc81b02ca8d5
OmegaAOL <hackersword666@gmail.com>
2026-06-11 03:42:26 -0700

    Fix issues with native version of <checkbox> not rendering properly

--- toolkit/content/widgets/checkbox.js
+++ toolkit/content/widgets/checkbox.js
@@ -55,6 +55,10 @@
       this.appendChild(this.constructor.fragment);
 
       this.initializeAttributeInheritance();
+	  
+	  if (!Services.prefs.getBoolPref("widget.non-native-theme.enabled", true)) {
+    this.setAttribute("native", "true");
+  }
     }
 
     set checked(val) {
